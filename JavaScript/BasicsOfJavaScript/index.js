a959b77e84e7b684bd60ad009ce5715e0a8a12a3
Riya415 <bansodriya415@gmail.com>
2025-11-17 17:37:46 +0530

    fromBootstap

---
+++ JavaScript/BasicsOfJavaScript/index.js
@@ -0,0 +1,17 @@
+
+let a = 10;
+console.log(a);
+let ar = ["Abc" , 12345 , 45.76 , 'a' , true];
+console.log(ar);
+alert("this is an alert")
+console.warn("this is warning ")
+console.error("this is an error")
+
+if(confirm("are you sure"))
+{
+    console.log("thanks");
+
+}
+else{
+    console.error("its okay");
+}
